bcaa61f541ef97d7320d4e46230a718d829205eb
Ben DeMoras <bd379@cornell.edu>
2026-01-16 13:45:49 -0600

    Include markdown Rmd output for Github

--- read-pesticide-data.R
+++
@@ -1,6 +0,0 @@
-# -----------------------------------------------------------------------------------
-# Load Packages
-# -----------------------------------------------------------------------------------
-library(tidyverse)   # Easier data manipulation
-library(readxl)      # Import Excel files directly
-
